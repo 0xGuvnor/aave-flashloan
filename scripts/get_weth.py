3a9cd0b24204f1e1f9f3f181ba55a1e2c1c1c185
0xbagholder <101540063+0xbagholder@users.noreply.github.com>
2022-04-13 12:29:31 +0800

    first commit

---
+++ scripts/get_weth.py
@@ -0,0 +1,22 @@
+from brownie import accounts, config, network, interface
+
+
+def main():
+    """
+    Runs the get_weth function to get WETH
+    """
+    get_weth(0.02)
+
+
+def get_weth(value=1):
+    """
+    Mints WETH by depositing ETH.
+    """
+    acct = accounts.add(
+        config["wallets"]["from_key"]
+    )  # add your keystore ID as an argument to this call
+    weth = interface.WethInterface(config["networks"][network.show_active()]["weth"])
+    tx = weth.deposit({"from": acct, "value": value * 10**18})
+    tx.wait(1)
+    print(f"Received {value} WETH")
+    return tx
